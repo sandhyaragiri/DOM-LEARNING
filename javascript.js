e3639d1bf99ab62ba3367e4dfd6a82fbedba7920
Sandhya <ragirisandhya24@gmail.com>
2026-04-13 20:52:31 +0530

    practiced DOM toggle

--- javascript.js
+++ javascript.js
@@ -1,5 +1,20 @@
-const myheading = document.querySelector("h1");
+/* const myheading = document.querySelector("h1");
 myheading.innerText = "Welcome to the club";
 
 
+const numbers = document.querySelectorAll("ul > li");
+numbers.forEach((item)=>{
+  item.style.background = "green";
+  item.style.color= "white";
+}); */
 
+function changeToggle(){
+  document.body.classList.toggle("darktoggle");
+  if(document.body.classList.contains("darktoggle")){
+    return document.getElementById("btnText").innerText= "☀️ Light Mode";
+  }
+
+  else{
+    return document.getElementById("btnText").innerText = "🌙 Dark Mode";
+  };
+}
