1dd9382c3b7517c4284cebc00449e1065ea9775d
Sandhya <ragirisandhya24@gmail.com>
2026-04-21 19:43:50 +0530

    created message displayer

--- javascript.js
+++ javascript.js
@@ -32,11 +32,81 @@ btn.addEventListener("click", changeText);
     }
   } */
 
-   let btn = document.querySelectorAll(".btn");
+  /*  let btn = document.querySelectorAll(".btn");
 
    btn.forEach((button)=>{
     button.addEventListener("click", function(e){
       e.target.innerText = "Clicked!";
     });
-   });
+   }); */
+
+
+   /* let btn = document.getElementById("btn");
+
+   btn.addEventListener("click", changeRed);
+   function changeRed(){
+    btn.style.backgroundColor = "red";
+   }
+
+   btn.addEventListener("mouseover", changeColor);
+    function changeColor(){
+
+      btn.style.backgroundColor = "blue";
+    }
+
+    btn.addEventListener("mouseout", changeGreen);
+    function changeGreen(){
+
+      this.style.backgroundColor = "green";
+    }
+
+    btn.addEventListener("click", changeText);
+    function changeText(){
+      this.innerText = "Clicked!";
+    }
+
+    btn.addEventListener("dblclick", reset);
+    function reset(){
+      this.innerText = "Enter";
+    } */
+   
+    /*   const btn = document.getElementById("btn");
+      btn.addEventListener("mouseover", function(){
+        btn.style.backgroundColor = "yellow";
+        btn.style.color = "black";
+      });
+
+      btn.addEventListener("mouseout", function(){
+        btn.style.backgroundColor = "purple";
+        btn.style.color = "white";
+      }); */
+
+      /* const btn = document.getElementById("btn"); */
+      const input = document.getElementById("input");
+      const output = document.getElementById("output");
+
+      input.addEventListener("input", function(){
+        if(input.value === ""){
+          output.innerText = "Start Typing bro";
+          output.style.color = "black";
+        }
+           
+         else if(input.value.length >= 10){
+            output.style.color = "red";
+            output.innerText = input.value;
+          }
   
+        else{
+             output.innerText = input.value;
+             output.style.color = "black";
+        }
+        
+      })
+
+
+
+      
+   
+      
+
+   
