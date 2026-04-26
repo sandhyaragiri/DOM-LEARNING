f56571ba7009fd5a44e02ca54e220d71fac8c2a4
Sandhya <ragirisandhya24@gmail.com>
2026-04-26 20:56:35 +0530

    creating login forms

--- javascript.js
+++ javascript.js
@@ -1,112 +1,40 @@
-/* const myheading = document.querySelector("h1");
-myheading.innerText = "Welcome to the club";
+const firstname = document.querySelector("#firstname");
+const lastname = document.querySelector("#lastname");
+const email = document.querySelector("#enterEmail");
+const password = document.querySelector("#password");
+const submit = document.querySelector("#submit");
 
+submit.addEventListener("click", loginSubmit);
+function loginSubmit(){
+  document.getElementById("fpara").innerText = "";
+  document.getElementById("Lpara").innerText = "";
+  document.getElementById("Epara").innerText = "";
+  document.getElementById("passwordP").innerText = "";
 
-const numbers = document.querySelectorAll("ul > li");
-numbers.forEach((item)=>{
-  item.style.background = "green";
-  item.style.color= "white";
-}); */
-
-/* function changeToggle(){
-  document.body.classList.toggle("darktoggle");
-  if(document.body.classList.contains("darktoggle")){
-    return document.getElementById("btnText").innerText= "☀️ Light Mode";
+  if(firstname.value === ""){
+    document.getElementById("fpara").innerText = "Enter first name";
   }
 
-  else{
-    return document.getElementById("btnText").innerText = "🌙 Dark Mode";
-  };
-} */
-
-/* let btn = document.getElementById("btn");
-
-btn.addEventListener("click", changeText);
-  function changeText(){
-    if(btn.innerText === "Click Me"){
-      btn.innerText = "Hello Sandhya!";
-    }
-
-    else {
-      btn.innerText = "Click Me";
-    }
-  } */
-
-  /*  let btn = document.querySelectorAll(".btn");
-
-   btn.forEach((button)=>{
-    button.addEventListener("click", function(e){
-      e.target.innerText = "Clicked!";
-    });
-   }); */
-
-
-   /* let btn = document.getElementById("btn");
-
-   btn.addEventListener("click", changeRed);
-   function changeRed(){
-    btn.style.backgroundColor = "red";
-   }
-
-   btn.addEventListener("mouseover", changeColor);
-    function changeColor(){
-
-      btn.style.backgroundColor = "blue";
-    }
-
-    btn.addEventListener("mouseout", changeGreen);
-    function changeGreen(){
-
-      this.style.backgroundColor = "green";
-    }
-
-    btn.addEventListener("click", changeText);
-    function changeText(){
-      this.innerText = "Clicked!";
-    }
-
-    btn.addEventListener("dblclick", reset);
-    function reset(){
-      this.innerText = "Enter";
-    } */
-   
-    /*   const btn = document.getElementById("btn");
-      btn.addEventListener("mouseover", function(){
-        btn.style.backgroundColor = "yellow";
-        btn.style.color = "black";
-      });
-
-      btn.addEventListener("mouseout", function(){
-        btn.style.backgroundColor = "purple";
-        btn.style.color = "white";
-      }); */
-
-      /* const btn = document.getElementById("btn"); */
-      const input = document.getElementById("input");
-      const output = document.getElementById("output");
-
-      input.addEventListener("input", function(){
-        if(input.value === ""){
-          output.innerText = "Start Typing bro";
-          output.style.color = "black";
-        }
-           
-         else if(input.value.length >= 10){
-            output.style.color = "red";
-            output.innerText = input.value;
-          }
+  if(lastname.value === ""){
+     document.getElementById("Lpara").innerText = "Enter last name";
+  }
   
-        else{
-             output.innerText = input.value;
-             output.style.color = "black";
-        }
-        
-      })
+  if(!email.value.includes("@")){
+    document.getElementById("Epara").innerText = `Error you missed the symbol ${"@"}`;
+    if(email.value === ""){
+      document.getElementById("Epara").innerText = "Hey You forget to enter email id";
+    }
+  }
 
+  if(password.value.length < 8){
+    document.getElementById("passwordP").innerText = "Password should be atlleast 8 characters";
+   if(password.value === ""){
+      document.getElementById("passwordP").innerText = "Password is Empty😐";
+    }
+  }
 
+  else {
+    document.getElementById("submit").innerText = "Login Successfully ✔"
+  }
 
-      
-   
-      
-
-   
+}
